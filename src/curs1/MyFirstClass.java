9e40a8939c0c481759af4c76fda9d304a218b225
unknown <gavriluta@gmail.com>
2022-09-08 21:40:12 +0300

    curs1

---
+++ src/curs1/MyFirstClass.java
@@ -0,0 +1,16 @@
+package curs1;
+
+public class MyFirstClass {
+	
+	String nume = "Dragos"; //variabila de instanta
+	int age = 23;
+
+	public static void main(String[] args) {
+		
+		MyFirstClass obj = new MyFirstClass();
+		
+    System.out.println("Salut " + obj.nume);
+    
+	}
+
+}
